85bf6fe6219a2f539acde29a6b844fdd018e8626
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-12-30 15:49:23 +0600

    initial commit

---
+++ src/main/java/com/asad/TestResponseApplication.java
@@ -0,0 +1,13 @@
+package com.asad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestResponseApplication.class, args);
+	}
+
+}
